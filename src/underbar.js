56a41f99825f12917c36002c27cf981e74d23e29
waynekwest88@gmail.com <waynekwest88@gmail.com>
2017-12-12 12:40:14 -0800

    memoize complete

--- src/underbar.js
+++ src/underbar.js
@@ -313,6 +313,19 @@
   // already computed the result for the given argument and return that value
   // instead if possible.
   _.memoize = function(func) {
+    var alreadyCalled = false;
+    var result;
+
+    return function() {
+      if (!alreadyCalled) {
+        result = func.apply(this, arguments);
+        alreadyCalled = true;
+      }
+      if (arguments.length > 1) {
+        result = func.apply(this, arguments);
+      }
+      return result;
+    };
   };
 
   // Delays a function for the given number of milliseconds, and then calls
